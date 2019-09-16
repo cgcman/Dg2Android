a88911fb60c214c058364c3944bb8a7e05b74bb8
Cgcman <info@grdj.com.ar>
2019-09-15 22:21:29 -0300

    cambios

--- app/src/main/java/com/grdj/Dg2Android/AuthActivity.java
+++ app/src/main/java/com/grdj/Dg2Android/AuthActivity.java
@@ -2,15 +2,33 @@ package com.grdj.Dg2Android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.RequestManager;
+
+import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
 public class AuthActivity extends DaggerAppCompatActivity {
 
+    @Inject
+    Drawable logo;
+
+    @Inject
+    RequestManager requestManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.auth_main);
+    }
+
+    private void setLogo(){
+        requestManager
+                .load(logo)
+                .into((ImageView)findViewById(R.id.login_logo));
     }
 }
